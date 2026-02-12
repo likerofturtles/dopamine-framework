1651b9f9bb4602916051bfb07e53f3f85692c0e0
likerofturtles <singhdivyam664@gmail.com>
2026-02-12 23:29:31 +0530

    fix: Fixed capitalization of Bot in legacy support import statement. Legacy support now works perfectly.

--- src/dopamine_framework/__init__.py
+++ src/dopamine_framework/__init__.py
@@ -1,5 +1,5 @@
 import warnings
-from src.dopamineframework import bot, now_plus_seconds_unix, duration_to_seconds, PrivateLayoutView, PrivateView, ViewPaginator, LayoutViewPaginator, mod_check, prefix_mod_check
+from src.dopamineframework import Bot, now_plus_seconds_unix, duration_to_seconds, PrivateLayoutView, PrivateView, ViewPaginator, LayoutViewPaginator, mod_check, prefix_mod_check
 
 warnings.warn(
     "dopamine_framework will be deprecated in the next major release. Use dopamineframework instead.",
